1c579742dab6f4f815533f2c457dd3e623dbed03
Lennard978 <shparavalo78nenad@gmail.com>
2025-12-10 10:06:19 +0100

    Design all pages

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,6 +1,6 @@
 // src/components/DarkModeToggle.jsx
 import React from "react";
-import { useTheme } from "../hooks/useTheme";
+import { useTheme } from "../hooks/useDarkMode";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 export default function DarkModeToggle() {
