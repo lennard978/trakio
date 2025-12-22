78696cfa010d99117834b62a0ff43cb4d03c0421
Lennard978 <shparavalo78nenad@gmail.com>
2025-12-22 13:50:05 +0100

    Dark Light Swipe

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+
+import "@fontsource/inter-tight/500.css";
+import "@fontsource/inter-tight/600.css";
+import "@fontsource/inter-tight/700.css";
+
 import {
   BrowserRouter,
   HashRouter
