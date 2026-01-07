22bfb669a8ccb4fb88e48580a1c03bff36c9f4e2
Lennard978 <shparavalo78nenad@gmail.com>
2026-01-08 00:59:48 +0100

    Service workers

--- src/main.jsx
+++ src/main.jsx
@@ -21,13 +21,13 @@ import { registerSW } from "virtual:pwa-register";
 import HardErrorBoundary
   from "./components/HardErrorBoundary.jsx";
 // Register PWA Service Worker asynchronously after render
-window.addEventListener("load", () => {
-  registerSW({ immediate: true });
-});
-if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-  registerSW({ immediate: true });
-}
-
+// window.addEventListener("load", () => {
+//   registerSW({ immediate: true });
+// });
+// if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+//   registerSW({ immediate: true });
+// }
+registerSW({ immediate: true });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HardErrorBoundary>
     <AppProviders>
